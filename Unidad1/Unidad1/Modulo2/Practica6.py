01dbae75a537570f437889f25aa44461199ea1ae
Josstop <jocelyngr750@gmail.com>
2024-09-30 15:43:35 -0600

    Modulo 2

---
+++ Unidad1/Unidad1/Modulo2/Practica6.py
@@ -0,0 +1,13 @@
+'''
+Autora: Jocelyn Gonzáles Ramírez
+Descripción: Observa el código en el editor: lee un valor flotante, lo coloca en una variable llamada x,
+e imprime el valor de la variable llamada y. Tu tarea es completar el código para evaluar la siguiente expresión:
+3x3 - 2x2 + 3x - 1
+2.4.1.10 LABORATORIO: Operadores y expresiones
+
+'''
+x =  -1 # codifica aquí tus datos de prueba
+x = float(x)
+# escribe tu código aquí
+y=3*(x**3)-2*(x**2)+3*(x)-1
+print("y =", y)
