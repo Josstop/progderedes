d6211cca33bf92aba64bfa4c924d7d1525d1608c
Josstop <jocelyngr750@gmail.com>
2024-09-30 15:42:54 -0600

    Modulo 2

---
+++ Unidad1/Unidad1/Modulo2/Practica5.py
@@ -0,0 +1,16 @@
+'''
+Autor: Jocelyn Gonzáles Ramírez
+Descripción: No se debe cambiar el código existente. Escribe tu código en los lugares indicados con ###. 
+Prueba tu programa con los datos que han sido provistos en el código fuente.
+2.4.1.9 LABORATORIO: Variables, un sencillo convertidor
+
+'''
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles*1.61
+kilometers_to_miles = kilometers/1.61
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
