ab416e27c6d76da622e3324f290d71ae9cc987fa
Josstop <jocelyngr750@gmail.com>
2024-10-03 15:01:24 -0600

    Ejercicio en clase

---
+++ Unidad1/Unidad1/Modulo3/Ejercicio1.py
@@ -0,0 +1,15 @@
+'''
+Autora: Jocelyn Gonzáles Ramírez
+Fecha: 03/10/2024
+Ejercicio 1 – Fundamentos de Python
+'''
+print('Autor: Jocelyn Gonzáles Ramírez\nEjercicio 1 – Fundamentos de Python')
+x = 4.0
+y = 3.0
+print("EL valor de la variable 1:", x,"\nEL valor de la variable 2:", y)
+
+print("La suma es:", x + y)
+print("La resta es:", x - y)
+print("La multiplicación es:", x * y)
+print("La división es:", x/y)
+print( "La potencia es:", x ** y)
--- Unidad1/Unidad1/Modulo3/Practica3.1.1.4.py
+++ Unidad1/Unidad1/Modulo3/Practica3.1.1.4.py
@@ -1,7 +1,6 @@
 '''
 Autora: Jocelyn Gonzales Ramírez
 Fecha: 24 de Septiembre del 2024
-
 3.1.1.4 LABORATORIO: Preguntas y respuestas
 Descripción: escribe un programa simple de dos líneas que tome el parámetro n como entrada, que es un entero, 
 e imprime False si n es menor que 100, y True si n es mayor o igual que 100.
