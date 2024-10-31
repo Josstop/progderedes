b470ac4d9da1fd4c62900ea6d67ba64f90707901
Josstop <jocelyngr750@gmail.com>
2024-10-31 13:38:47 -0600

    unidad 2

--- Unidad2/U2EJER02_JGR.py
+++ Unidad2/U2EJER02_JGR.py
@@ -1,36 +1,48 @@
 import requests
+
 '''
 API DE BÚSQUEDA DE TRAGOS
 Alumna: Jocelyn Gonzáles Ramírez 
 Unidad 2. Ordinario
-
 '''
+
 while True: 
-   # url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
     print("Bienvenido a las recetas de cocteles:")
-    #print("Menú de bebidas: "
-    #     "1. M A R G A R I T A"
-    #     "2. BLUE MARGARITA"
-    #      "3. TOMMYS MARGARITA"
-    #      "4. Whitecap Margarita"
-    #      "2. BLUE MARGARITA"
-    #       
-    #     )
-    #data =requests.get(url).json()
-    nombreBebida= input("¿Que bebida deseas buscar?")
+    print("Menú de bebidas:\n"
+          "1. MARGARITA\n"
+          "2. BLUE MARGARITA\n"
+          "3. TOMMY'S MARGARITA\n"
+          "4. WHITECAP MARGARITA\n"
+          "5. SALIR\n")
+    
+    nombreBebida = input("¿Qué bebida deseas buscar? (O escribe 'salir' para terminar): ").strip()
+    
+    if nombreBebida.lower() == "salir":
+        print("Gracias por usar el buscador de cocteles. ¡Hasta la próxima!")
+        break
+    
     url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={nombreBebida}"
     bebida = requests.get(url)
-    if nombreBebida == 200:
-       # if nombreBebida ==  "Margarita":
+    
+    if bebida.status_code == 200:
         datos = bebida.json()
-        if datos["drinks"]:
-            for bebida in datos["drinks"]:
-                # Mostramos el nombre de la bebida
-                print("Nombre de la bebida:", bebida["strDrink"])
-                # Opcionalmente, mostramos otros detalles
-                print("Categoría:", bebida["strCategory"])
-                print("Tipo de vaso:", bebida["strGlass"])
-                print("Instrucciones:", bebida["strInstructions"])
-                print("Ingredientes:")
+        # Verificamos si hay datos para mostrar
+        if datos['drinks']:
+            #buscar = datos.json()
+            res = input("Que quieres saber de la bebida:\na)Categoria\nb)Ingredientes\nc)Tipo de vaso\nd)Insrucciones de preparación")
+            res.lower()
+            if res == 'a':
+              i = bebida
+              for i in 'drinks':
+                categoria = datos['drinks'][0]['strCategory']
+                print(f"Categoria: {categoria}")
+
+
+            #print("Receta encontrada:")
+            #print(datos)  # O puedes acceder a datos específicos en `datos['drinks']`
+            #while True:
+            
+        else:
+            print("No se encontró información sobre esa bebida.")
     else:
-        print("Esa bebida no se encuentra")
+        print("Error en la búsqueda de la bebida. Por favor, intenta de nuevo.")
